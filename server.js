92ae56ded5273e76fc57ecc2a66fcb14f08604e9
Carmen Chow <carmendevelops@gmail.com>
2023-01-02 23:05:51 -0500

    get route name

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
 
-console.log(app); // app object
+// console.log(app); // app object
+
+app.get('/', (req, res) => {
+  // res.send('Hello world!');
+  res.json({ name: 'Carmen', age: 41 });
+});
+
+app.get('/names/:name', (req, res) => {
+  res.send(`Hello ${req.params.name}`);
+});
 
 
 app.listen(8000);
